fa334e448d75907aaac9e49f83f1b6e908ee07ea
singularis <singularis314@gmail.com>
2021-10-09 23:41:15 +0300

    Update setup-kubetools.sh

--- setup-kubetools.sh
+++ setup-kubetools.sh
@@ -16,30 +16,31 @@ MYOS=$(hostnamectl | awk '/Operating/ { print $3 }')
 OSVERSION=$(hostnamectl | awk '/Operating/ { print $4 }')
 
 ##### CentOS 7 config
-if [ $MYOS = "centos" ]
+if [ $MYOS = "CentOS" ]
 then
 	echo RUNNING CENTOS CONFIG
-	cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+	cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 	[kubernetes]
 	name=Kubernetes
-	baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+	baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
 	enabled=1
 	gpgcheck=1
 	repo_gpgcheck=1
 	gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
+	exclude=kubelet kubeadm kubectl
+	EOF
 
 	# Set SELinux in permissive mode (effectively disabling it)
-	setenforce 0
-	sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+	sudo setenforce 0
+	sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
 	# disable swap (assuming that the name is /dev/centos/swap
 	sed -i 's/^\/dev\/mapper\/centos-swap/#\/dev\/mapper\/centos-swap/' /etc/fstab
 	swapoff /dev/mapper/centos-swap
 
-	yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+	sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
-	systemctl enable --now kubelet
+	sudo systemctl enable --now kubelet
 fi
 
 if [ $MYOS = "Ubuntu" ]
